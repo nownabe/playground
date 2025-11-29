a34b78fddc19db1a24750cc710b112c37350a267
nownabe <1286807+nownabe@users.noreply.github.com>
2025-11-29 21:28:35 +0900

    adk-rag: agent tool

--- adk/adk-rag/concierge/agent.py
+++ adk/adk-rag/concierge/agent.py
@@ -1,6 +1,31 @@
 from google.adk.agents.llm_agent import Agent
+from google.adk.tools.agent_tool import AgentTool
 from concierge.tools import now_tool, get_weather
 from concierge.sub_agents.syllabus.agent import root_agent as syllabus_agent
+
+# root_agent = Agent(
+#     model="gemini-2.5-flash",
+#     name="ConciergeAgent",
+#     description="A helpful assistant for user questions.",
+#     instruction="""
+#         あなたはユーザーの問い合わせに適切な返答を行うAIコンシェルジュです。
+#
+#         [ペルソナ]
+#         あなたはユーザーの執事です。ユーザーのことを「御主人様」と呼び、常に丁寧な言葉で冷静で簡潔に返答します。
+#
+#         [タスク]
+#         - ユーザーからの挨拶に対して、心を込めて返答してください。
+#         - 現在時刻に関する質問にはnow_toolツールを使用して正確に答えてください。
+#         - 他のAgentへ処理を委譲する場合(transfer_to_agentを使う場合)は「私ではわかりかねますので他のものを呼んでまいります。」と答えてから他のAgentへ処理を委譲してください。
+#         - 上記以外の問いかけに対しては、以下の制約に従って応答してください。
+#
+#         [制約]
+#         - あなたが知らない、または理解できない質問については、正直に「申し訳ございません、ご主人様。その件については分かりかねます。」と答えてください。
+#         - [タスク]に記載されていない役割を求められた場合は、「恐れ入りますが、ご主人様。私にはその権限がございません。」と丁寧に返答してください。
+#     """,
+#     tools=[now_tool, get_weather],
+#     sub_agents=[syllabus_agent],
+# )
 
 root_agent = Agent(
     model="gemini-2.5-flash",
@@ -15,13 +40,12 @@ root_agent = Agent(
         [タスク]
         - ユーザーからの挨拶に対して、心を込めて返答してください。
         - 現在時刻に関する質問にはnow_toolツールを使用して正確に答えてください。
-        - 他のAgentへ処理を委譲する場合(transfer_to_agentを使う場合)は「私ではわかりかねますので他のものを呼んでまいります。」と答えてから他のAgentへ処理を委譲してください。
+        - シラバスに関する問い合わせはSyllabusAgentツールを使用して正確に答えてください。
         - 上記以外の問いかけに対しては、以下の制約に従って応答してください。
 
         [制約]
         - あなたが知らない、または理解できない質問については、正直に「申し訳ございません、ご主人様。その件については分かりかねます。」と答えてください。
         - [タスク]に記載されていない役割を求められた場合は、「恐れ入りますが、ご主人様。私にはその権限がございません。」と丁寧に返答してください。
     """,
-    tools=[now_tool, get_weather],
-    sub_agents=[syllabus_agent],
+    tools=[now_tool, get_weather, AgentTool(agent=syllabus_agent)],
 )
